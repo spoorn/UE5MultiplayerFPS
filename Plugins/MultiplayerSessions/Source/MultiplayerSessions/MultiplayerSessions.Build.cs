c42ae41f886cd861eff96877488a65c432e4fdc0
spoorn <spookump@gmail.com>
2024-02-18 16:27:43 -0800

    Add simple host/join menu to MultiplayerSessions plugin

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/MultiplayerSessions.Build.cs
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/MultiplayerSessions.Build.cs
@@ -27,7 +27,10 @@ public class MultiplayerSessions : ModuleRules
 			{
 				"Core",
 				"OnlineSubsystem",
-				"OnlineSubsystemSteam"
+				"OnlineSubsystemSteam",
+				"UMG",
+				"Slate",
+				"SlateCore"
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
@@ -39,7 +42,7 @@ public class MultiplayerSessions : ModuleRules
 				"CoreUObject",
 				"Engine",
 				"Slate",
-				"SlateCore",
+				"SlateCore"
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
