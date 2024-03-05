8e476540abfa2ac77eef4bc1658479c0ee054cbe
spoorn <spookump@gmail.com>
2024-03-05 01:01:54 -0800

    Create blanket Projectile with CollisionBox and ProjectileWeapon

---
+++ Source/Blaster/Public/Weapon/ProjectileWeapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon/Weapon.h"
+#include "ProjectileWeapon.generated.h"
+
+/**
+ * Weapon that spawns projectiles
+ */
+UCLASS()
+class BLASTER_API AProjectileWeapon : public AWeapon
+{
+	GENERATED_BODY()
+	
+};
